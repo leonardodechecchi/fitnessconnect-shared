83462176472904b14b73331ed5f08b24d2236a17
Leonardo De Checchi <leo.dechi99@gmail.com>
2024-12-24 00:10:19 +0100

    feat: add export for wishlist-item schema

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './user/user.schemas.js';
 export * from './user/trainer/trainer.schemas.js';
 export * from './wishlist/wishlist.schemas.js';
+export * from './wishlist/wishlist-item.schema.js';
 export * from './auth/auth.schemas.js';
