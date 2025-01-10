b18e784d7d4668b72651f7a0a9c424a239d41442
Leonardo De Checchi <leo.dechi99@gmail.com>
2025-01-10 16:38:06 +0100

    Forgot to add export

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './trainer/trainer.schemas.js';
 export * from './wishlist/wishlist.schemas.js';
 export * from './wishlist/wishlist-item.schema.js';
 export * from './auth/auth.schemas.js';
+export * from './review/review.schemas.js';
